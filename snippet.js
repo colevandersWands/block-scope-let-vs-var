0e04928f73aec72b8455cec1f64a4e4e9cfdf8b2
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-08-09 05:07:28 +0200

    aaa_var

--- snippet.js
+++ snippet.js
@@ -9,5 +9,5 @@ console.log('leaving outer scope');
     console.log('leaving block scope');
 };
 console.log('re-entering outer scope');
-inner_var = 'modified globally';
+aaa_var = 'modified globally';
 console.log('final state');
